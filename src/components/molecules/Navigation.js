69a011261eafa35903ec303e138fdd7b14e2988c
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2019-12-23 14:29:00 +0000

    Added Navigation

---
+++ src/components/molecules/Navigation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/community">
+                        Community
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
