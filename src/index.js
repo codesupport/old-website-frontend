2ed4d586785f5373cd13b69362de1377081abe59
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2019-12-23 14:32:21 +0000

    Added space after Navigation.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Navigation from "./components/molecules/Navigation";
 function Router() {
     return (
         <BrowserRouter>
-            <Navigation/>
+            <Navigation />
             <Switch>
                 <Route exact path="/" component={Index} />
                 <Route exact path="/community" component={Community} />
