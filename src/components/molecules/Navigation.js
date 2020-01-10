065760c2f0468989915f9db2f8f7a9b44e259722
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-01-10 20:29:20 +0000

    Added Modal to Navigation.

--- src/components/molecules/Navigation.js
+++ src/components/molecules/Navigation.js
@@ -1,39 +1,66 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
 
-import "../../css/molecules/navigation.css";
 import config from "../../config";
 
-function Navigation() {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <img
-                        className="logo"
-                        alt="CodeSupport Logo"
-                        src={`${config["firebase-bucket-url"]}/logo.png?alt=media`}
-                        draggable="false"
-                    />
-                </li>
-                <li>
-                    <Link to="/">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/community">
-                        Community
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/resources">
-                        Resources
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    );
+import SignUpForm from "../organisms/SignUpForm";
+import ModalContainer from "../templates/ModalContainer";
+
+import "../../css/molecules/navigation.css";
+
+class Navigation extends Component {
+    state = {
+        showModal: false,
+        loggedIn: false
+    };
+
+    toggleLoginModal = () => {
+        const {showModal} = this.state;
+
+        this.setState({
+            showModal: !showModal
+        });
+    };
+
+    render() {
+        const {showModal, loggedIn} = this.state;
+
+        return (
+            <>
+                {showModal ? <ModalContainer> <SignUpForm/> </ModalContainer> : ""}
+                <nav>
+                    <ul>
+                        <li>
+                            <img
+                                className="logo"
+                                alt="CodeSupport Logo"
+                                src={`${config["firebase-bucket-url"]}/logo.png?alt=media`}
+                                draggable="false"
+                            />
+                        </li>
+                        <li>
+                            <Link to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/community">
+                                Community
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/resources">
+                                Resources
+                            </Link>
+                        </li>
+                        <li className="login" onClick={this.toggleLoginModal}>
+                            {loggedIn ? "Log Out" : "Log In"}
+                        </li>
+                    </ul>
+                </nav>
+            </>
+        );
+    }
 }
 
 export default Navigation;
