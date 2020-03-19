0ad65149307e6be95e7b477ea506f41abe15c306
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-03-19 17:40:52 +0000

    Made ShowcaseService.getAllPosts() return a more friendly response.

--- src/services/ShowcaseService.js
+++ src/services/ShowcaseService.js
@@ -9,7 +9,20 @@ class ShowcaseService {
             const {data} = await axios.get(this.api);
 
             if (data.status === "OK") {
-                return data.response;
+                return data.response[0].map((post) => ({
+                    id: post.id,
+                    title: post.title,
+                    description: post.description,
+                    approved: post.approved,
+                    user: {
+                        id: post.user.id,
+                        alias: post.user.alias
+                    },
+                    contributors: post.contributorList.contributors.map((contributor) => ({
+                       id: contributor.user.id,
+                       alias: contributor.user.alias
+                    }))
+                }));
             } else {
                 return [];
             }
