4d4ab3ef877356005e5772054a6b989a2a83ca75
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-06-06 17:47:55 +0100

    Added social media icons to navigation.

--- src/components/molecules/Navigation.js
+++ src/components/molecules/Navigation.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import config from "../../config";
 
 function Navigation() {
     return (
         <nav className="uk-navbar-container" uk-navbar>
-            <div className="uk-navbar-center">
+            <div className="uk-navbar-left">
                 <ul className="uk-navbar-nav nav-logo">
                     <Link to="/">
                         <img
@@ -21,14 +23,28 @@ function Navigation() {
                         </Link>
                     </li>
                     <li>
-                        <Link to="/community">
-                            Community
-                        </Link>
-                    </li>
-                    <li>
                         <Link to="/resources">
                             Resources
                         </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="nav-social uk-navbar-right">
+                <ul className="uk-navbar-nav">
+                    <li>
+                        <a href="https://codesupport.dev/discord">
+                            <FontAwesomeIcon icon={faDiscord} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/codesupportdev">
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </a>
+                    </li>
+                    <li className="uk-margin-right">
+                        <a href="https://github.com/codesupport">
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
                     </li>
                 </ul>
             </div>
