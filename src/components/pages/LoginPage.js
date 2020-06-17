4e40f94111031ab8ea88a69b57b4d4e1eb6c2daa
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-06-17 16:14:07 +0100

    Add styling to the login page to make it more appealing.

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PageTemplate from "../templates/PageTemplate";
+import Card from "../molecules/Card";
+import LogInForm from "../organisms/LogInForm";
+import "../../css/pages/loginpage.css";
 
 function LoginPage() {
     return (
         <PageTemplate page="Log In">
+            <section className="login-area">
+                <Card title="Log In" description="Log into your CodeSupport account to start interacting with the community.">
+                    <LogInForm />
+                    <p className="login-no-account">
+                        <Link to="/signup">Not got an account yet?</Link>
+                    </p>
+                </Card>
+            </section>
         </PageTemplate>
     );
 }
